bedfd10c7c3b9fc184bc73573418112bf6eb5f97
Aske <aske.funder@gmail.com>
2018-12-04 14:36:12 +0100

    Create
    create er lavet

--- src/main/java/com/example/demo/controller/FoodItemController.java
+++ src/main/java/com/example/demo/controller/FoodItemController.java
@@ -1,11 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.FoodItemModel;
 import com.example.demo.service.FoodItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping ("fooditem/")
 public class FoodItemController {
 
     @Autowired
@@ -14,6 +20,18 @@ public class FoodItemController {
     @GetMapping (value = "/")
     public String homePage(){
         return "index";
+    }
+
+    @GetMapping (value = "/create")
+    public String create(Model model){
+        model.addAttribute("foodItemModel", new FoodItemModel());
+        return "fooditem/create";
+    }
+
+    @PostMapping ("/save")
+    public String save(@ModelAttribute FoodItemModel foodItemModel){
+        foodItemService.save(foodItemModel);
+        return "redirect:/fooditem/";
     }
 
 }
--- src/main/java/com/example/demo/service/FoodItemService.java
+++ src/main/java/com/example/demo/service/FoodItemService.java
@@ -1,7 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.model.FoodItemModel;
+import com.example.demo.repository.FoodItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FoodItemService {
+
+    @Autowired
+    FoodItemRepository foodItemRepository;
+
+    public void save(FoodItemModel foodItemModel) {
+        foodItemRepository.save(foodItemModel);
+    }
 }
