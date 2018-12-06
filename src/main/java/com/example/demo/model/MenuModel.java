69dfa36333fd8154438c044583359a6ee3764bfa
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-12-06 14:22:06 +0100

    menu create now works with basic information

--- src/main/java/com/example/demo/model/MenuModel.java
+++ src/main/java/com/example/demo/model/MenuModel.java
@@ -33,4 +33,44 @@ public class MenuModel {
         foodItemModel.getMenus().remove(this);
 
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<FoodItemModel> getFoodItems() {
+        return foodItems;
+    }
+
+    public void setFoodItems(List<FoodItemModel> foodItems) {
+        this.foodItems = foodItems;
+    }
 }
