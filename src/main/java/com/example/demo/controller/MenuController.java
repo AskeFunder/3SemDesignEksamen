80702e01d3ea155a703ef583514f9724762aeba5
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-12-12 09:15:54 +0100

    cleaned up the food-item/index and menu/index in tabels and header

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -24,28 +24,27 @@ public class MenuController {
 
     @GetMapping
     public String menuPage(Model model, @RequestParam(defaultValue = "0") int page) {
-        model.addAttribute("menus", menuService.listAll(page) );
-        model.addAttribute("currentPage",page);
+        model.addAttribute("menus", menuService.listAll(page));
+        model.addAttribute("currentPage", page);
         return "menu/index";
     }
 
     @GetMapping("/user-ui")
-    public String userUI(Model model, @RequestParam(defaultValue = "0") int page){
+    public String userUI(Model model, @RequestParam(defaultValue = "0") int page) {
         model.addAttribute("menus", menuService.listAll(page));
         model.addAttribute("currentPage", page);
         return "menu/user-menu";
     }
 
     @GetMapping("/create")
-    public String menuCreatePage(Model model){
+    public String menuCreatePage(Model model) {
         model.addAttribute("menuModel", new MenuModel());
-        model.addAttribute("foodItem",foodItemService.listAll());
+        model.addAttribute("foodItem", foodItemService.listAll());
         return "menu/create";
     }
 
     @PostMapping("/save")
-    public String saveCourse(@ModelAttribute MenuModel menuModel)
-    {
+    public String saveCourse(@ModelAttribute MenuModel menuModel) {
         menuService.save(menuModel);
         return "redirect:/menu/";
     }
@@ -55,30 +54,31 @@ public class MenuController {
     @ResponseBody
     public MenuModel findOne(Integer id) {
         return menuService.findMenuById(id);
+    }
+        @GetMapping("/edit")
+        public String edit (Model model,int id){
+            model.addAttribute("menuModel", menuService.getOne(id));
+            model.addAttribute("foodItems", foodItemService.listAll());
+            return "menu/edit";
+        }
 
-    @GetMapping("/edit")
-    public String edit(Model model, int id) {
-        model.addAttribute("menuModel", menuService.getOne(id));
-        model.addAttribute("foodItems", foodItemService.listAll());
-        return "menu/edit";
+        @PutMapping("/edit/save/")
+        public String saveEditMenu (@ModelAttribute MenuModel menuModel,int id){
+            menuService.editMenu(menuModel, id);
+            return "redirect:/menu";
+        }
+
+        @GetMapping("/delete")
+        public MenuModel menuDeletePage ( int id)
+        {
+            return menuService.getOne(id);
+        }
+
+        @DeleteMapping("/delete/confirm")
+        public String deleteConfirmed (@ModelAttribute MenuModel menuModel,int id){
+            menuService.delete(menuModel, id);
+            return "redirect:/food-item";
+
+        }
     }
 
-    @PutMapping("/edit/save/")
-    public String saveEditMenu(@ModelAttribute MenuModel menuModel, int id) {
-        menuService.editMenu(menuModel, id);
-        return "redirect:/menu";
-    }
-
-    @GetMapping("/delete")
-    public MenuModel menuDeletePage(int id)
-    {
-        return menuService.getOne(id);
-    }
-
-    @DeleteMapping("/delete/confirm")
-    public String deleteConfirmed(@ModelAttribute MenuModel menuModel, int id) {
-        menuService.delete(menuModel, id);
-        return "redirect:/food-item";
-
-    }
-}
